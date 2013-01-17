70acd52cf955a5863270f590ab45dcaf2c2b4d55
pinguet62 <pinguet62@gmail.com>
2013-01-17 17:37:02 +0100

    Ajout rep project

---
+++ project/Build.scala
@@ -0,0 +1,29 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "Croquette"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+	//groupid % artifact % version
+    javaCore,
+    javaJdbc,
+    javaEbean,
+	//for XMPP
+	//"jivesoftware" % "smack" % "3.2.1",
+    "org.igniterealtime.smack" % "smack" % "3.2.1",
+	//to retrieve contact from google
+	"com.google.gdata" % "core" % "1.47.1",
+	//to capitalize name
+	"commons-lang" % "commons-lang" % "2.5"
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
+
+}
